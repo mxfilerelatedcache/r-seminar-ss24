6c7413fd4f39ba0ff56770f49cd97850881ecf1a
mxfilerelatedcache <sk@rias-institute.de>
2023-05-03 16:06:42 +0200

    removed random git folders

--- 4056482c/assets/scripts/termin1.R
+++
@@ -1,5 +0,0 @@
-# Erstes Skript zum Seminar Medienbasierte Wissenskonstruktion: R
-
-# Satz in Konsole printen:
-print(paste0("Ich habe ",R.version$version.string," und RStudio v",rstudioapi::versionInfo()$version))
-
