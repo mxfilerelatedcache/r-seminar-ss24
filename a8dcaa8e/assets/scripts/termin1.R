1415ffd87825217a7a083fd9fe1bed2b944cf9f9
mxfilerelatedcache <sk@rias-institute.de>
2023-05-04 11:57:02 +0200

    Built site for gh-pages

---
+++ a8dcaa8e/assets/scripts/termin1.R
@@ -0,0 +1,5 @@
+# Erstes Skript zum Seminar Medienbasierte Wissenskonstruktion: R
+
+# Satz in Konsole printen:
+print(paste0("Ich habe ",R.version$version.string," und RStudio v",rstudioapi::versionInfo()$version))
+
